23d49ef8529e82dfdcad41db9fbd51c923236a3c
Narendra Jadhav <narendraj9193@gmail.com>
2023-09-04 06:16:44 +0530

    Github link change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
         <nav className="flex justify-between items-center w-full mb-10 pt-3">
             <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
 
-            <button type="button" onClick={() => window.open('https://github.com')} className="black_btn">
+            <button type="button" onClick={() => window.open('https://github.com/Narendra-Jadhav/AI-Summarizer')} className="black_btn">
                 GitHub
             </button>
         </nav>
